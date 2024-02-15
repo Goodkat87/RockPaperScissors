8cd0d5be06aff25aaf16a8f730d586a7f88d7c3a
Goodkat87 <ilyass.zrl@hotmail.com>
2024-02-15 09:44:40 +0100

    add of all images

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,18 @@
 import { useState } from 'react'
+import Rules from './assets/img/image-rules.svg'
+import Paper from "./assets/img/icon-paper.svg"
+import Rock from "./assets/img/icon-rock.svg"
+import Scissors from "./assets/img/icon-scissors.svg"
+import Close from "./assets/img/icon-close.svg"
+import Triangle from "./assets/img/bg-triangle.svg"
+import Logo from "./assets/img/logo.svg"
 
 function App() {
 
   return (
-    <div>
-
+    <div className='h-screen w-screen bg-gradient-to-b from-[#1f3756] to-[#141539]'>
+      
+      
     </div>
   )
 }
