a4757d92135844f5940896003656501a0ef239cc
Goodkat87 <ilyass.zrl@hotmail.com>
2024-02-15 10:01:25 +0100

    add of font

--- src/components/Title.jsx
+++ src/components/Title.jsx
@@ -4,6 +4,10 @@ export default function Title(props) {
   return (
     <div className='w-1/2 h-40 border-[4px] border-opacity-15 border-white rounded-xl flex items-center'>
         <img src={props.logo} alt="logo"/>
+        <div className='h-[99px] w-[130px] bg-white rounded-lg flex flex-col items-center justify-center'>
+            <p>Score</p>
+            <h1>12</h1>
+        </div>
     </div>
   )
 }
