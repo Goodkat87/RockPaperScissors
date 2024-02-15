84c953498daa06b497241b1550a8bff54b1fe0f6
Goodkat87 <ilyass.zrl@hotmail.com>
2024-02-15 10:13:09 +0100

    end of design for Title components

--- src/components/Title.jsx
+++ src/components/Title.jsx
@@ -2,11 +2,13 @@ import React from 'react'
 
 export default function Title(props) {
   return (
-    <div className='w-1/2 h-40 border-[4px] border-opacity-15 border-white rounded-xl flex items-center'>
-        <img src={props.logo} alt="logo"/>
-        <div className='h-[99px] w-[130px] bg-white rounded-lg flex flex-col items-center justify-center'>
-            <p>Score</p>
-            <h1>12</h1>
+    <div className='w-2/5 h-40 border-[4px] border-opacity-15 border-white rounded-xl flex justify-center'>
+        <div className='h-full w-11/12 flex items-center justify-between'>
+            <img src={props.logo} alt="logo"/>
+            <div className='h-[99px] w-[130px] bg-white rounded-lg flex flex-col items-center justify-center'>
+                <p className='font-medium text-[18px] text-blue-700 '>Score</p>
+                <h1 className='text-[40px] font-bold'>12</h1>
+            </div>
         </div>
     </div>
   )
